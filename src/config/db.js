260557d97e3a3471b6863be20f4eb1206f6e42c5
Aloïs <aloiiiiiiis@gmail.com>
2020-01-23 18:27:09 +0100

    Update app.js and db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,5 @@
 const Sequelize = require('sequelize')
 
-// Option 1: Passing parameters separately
 const sequelize = new Sequelize(process.env.NAME_DATABASE, process.env.USERNAME_DATABASE, process.env.PASSWORD_DATABASE, {
   host: process.env.HOST_DATABASE,
   port: process.env.PORT_DATABASE,
@@ -8,9 +7,3 @@ const sequelize = new Sequelize(process.env.NAME_DATABASE, process.env.USERNAME_
 })
 
 module.exports = sequelize
-
-/*
-  const sequelize = new Sequelize('publicationdb', 'meltingpotes', 'Y-_0J34q5F&Gkd;vcUy*', {
-  host: '51.159.25.15',
-  port: '37695',
-  */
