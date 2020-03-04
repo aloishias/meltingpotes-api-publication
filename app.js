103b746e6fb0407f06e9f28c3ec382494d1c4b96
aloishias <aloiiiiiiis>
2020-03-04 11:54:56 +0100

    Update port of running API

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const port = 3100
+const port = process.env.PORT || 3100
 const cors = require('cors')
 
 const publicationroutes = require('./src/routes/Publication.js')
